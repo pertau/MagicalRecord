4a5f243e5f43fb30184eed89bae7a48e711a9482
Per Tautorat <per.tautorat@allianz.de>
2024-04-02 08:40:41 +0200

    static version number to fix build issue

--- Support/Scripts/set_version_information.sh
+++ Support/Scripts/set_version_information.sh
@@ -10,7 +10,7 @@ dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${clear_release_version#*v}" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion 1" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 2.3.3" "$plist"
   fi
 done
